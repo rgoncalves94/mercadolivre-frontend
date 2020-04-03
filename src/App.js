41a236de115338178629946667c53c4e89887709
Rafael Gonçalves <r.goncalves94@hotmail.com>
2020-04-02 21:52:49 -0300

    Applying the store in the application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.scss';
 import Routes from './routes'
+import { Provider } from 'react-redux';
+import { Store } from './state/createStore';
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <Provider store={Store}>
+      <div className="App">
+        <Routes />
+      </div>
+    </Provider>
   );
 }
 
