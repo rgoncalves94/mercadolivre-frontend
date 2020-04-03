ddd5b9af12023f215a5c3ea789eb79830f4bb51c
Rafael Gonçalves <r.goncalves94@hotmail.com>
2020-04-02 21:46:10 -0300

    logo component redirects when it's clicked

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './styles.scss'
 import logo from '../../assets/Logo_ML.png'
-
+import history from '../../history'
 
 function Logo() {
     return (
         <img 
+            className="logo"
+            onClick={() => history.push('/')}
             src={logo} 
             title="Logotipo do Mercado Livre" 
             alt="Logotipo do Mercado Livre" />
